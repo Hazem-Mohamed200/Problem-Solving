42b5c0f7d1908738817c37ace8015886089bea64
Hazem Mohamed <hazemshata200@gmail.com>
2021-04-23 04:15:00 +0200

    Add solution for problem `A - Boring Apartments` Link: https://codeforces.com/contest/1433/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1433/submission/113933450

---
+++ codeforces/1433/A.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define el '\n'
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pld pair<ld,ld>
+#define pff pair<float,float>
+#define F first
+#define S second
+#define vec(int) vector<int>
+#define pb push_back
+#define pf push_front
+#define mms(v,i) memset(v, i, sizeof v)
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+int dx[]= {0, 1, 0, -1};
+int dy[]= {1, 0, -1, 0};
+const ll MAX = 500100; const ll mod = 1000000007;
+using namespace std;
+
+void file()
+{
+    freopen("input.txt","r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+}
+void zuka()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+int main()
+{
+    zuka();
+    int t; cin >> t;
+    while(t--)
+    {
+        int x; cin >> x;
+        string s = "1";
+        char c = '1';
+        int ans = 0;
+        while(true)
+        {
+            //cout << s << el;
+            ans += s.size();
+            if(s == to_string(x))
+            {
+                cout << ans << el;
+                break;
+            }
+            if(s.size() < 4)
+                s+=c;
+            else
+            {
+                c++;
+                s = c;
+            }
+
+        }
+    }
+}
