384377c60103ce7293dfe4043d8a84d423f1eb64
Hazem Mohamed <hazemshata200@gmail.com>
2021-04-23 20:57:00 +0200

    Add solution for problem `A - Sum of 2050` Link: https://codeforces.com/contest/1517/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1517/submission/114049804

---
+++ codeforces/1517/A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#define el '\n'
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pld pair<ld,ld>
+#define pff pair<float,float>
+#define F first
+#define S second
+#define vec(int) vector<int>
+#define pb push_back
+#define pf push_front
+#define mms(v,i) memset(v, i, sizeof v)
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+int dx[]= {0, 1, 0, -1};
+int dy[]= {1, 0, -1, 0};
+const ll MAX = 500100; const ll mod = 1000000007;
+using namespace std;
+
+void file()
+{
+    freopen("input.txt","r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+}
+void zuka()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+ll solve(ll num)
+{
+    ll x = 0;
+    num /=2050;
+   while(num)
+   {
+       x += num%10;
+       num/=10;
+   }
+    return x;
+}
+int main()
+{
+    zuka();
+    int t; cin >> t;
+    while(t--)
+    {
+        ll n; cin >> n;
+        if(n%2050 == 0)
+        {
+            cout << solve(n) << el;
+        }
+        else
+            cout << -1 << el;
+    }
+}
