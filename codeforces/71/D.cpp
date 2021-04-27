969ee42488635a75e39d759a18816150a4aa5908
Hazem Mohamed <hazemshata200@gmail.com>
2021-04-27 22:51:00 +0200

    Add solution for problem `D - Solitaire` Link: https://codeforces.com/contest/71/problem/D Tags: brute force, implementation, *2200 Ref: https://codeforces.com/contest/71/submission/114399207

--- codeforces/71/D.cpp
+++ codeforces/71/D.cpp
@@ -30,11 +30,7 @@ void zuka()
     cout.tie(0);
 }
 int n,m;
-int ansy1,ansy2,ansx1,ansx2;
-string joker1,joker2;
 string input[N][N] = {};
-vector<pair<string,pii>>jokers;
-map<pii,vector<pii>>squares;
 bool overlap(int i1, int j1, int i2, int j2)
 {
     if (i2 > i1 + 2 || i1 > i2 + 2)
@@ -62,7 +58,7 @@ set<string>pre()
 }
 map<pii,vector<pii>>pre2()
 {
-    map<pii,vector<pii>>group;
+    map<pii,vector<pii>>squares;
     for(int i = 1; i <= n-2; i++)
     {
         for(int j = 1; j <= m-2; j++)
@@ -75,7 +71,7 @@ map<pii,vector<pii>>pre2()
                     {
                         //First Square
                         map<char,int>freq1,freq2;
-                        bool s1 = true,s2 = true; //Check the validity of the two group
+                        bool s1 = true,s2 = true; //Check the validity of the two squares
                         for(int y = i; y <= i + 2; y++)
                         {
                             for(int x = j; x <= j + 2; x++)
@@ -96,7 +92,7 @@ map<pii,vector<pii>>pre2()
                         for(auto x:freq1)
                         {
                             if(x.S > 1)
-                            {s1 = false;break;}
+                                {s1 = false;break;}
                         }
                         for(auto x:freq2)
                         {
@@ -104,39 +100,19 @@ map<pii,vector<pii>>pre2()
                             {s2 = false;break;}
                         }
                         if(s1 && s2)
-                            group[{i, j}].pb({si, sj});
+                            squares[{i,j}].pb({si,sj});
                     }
                 }
             }
         }
     }
-    return group;
-}
-void output(int j,bool can)
-{
-    if(can)
-    {
-        cout << "Solution exists.\n";
-        if(j == 2)
-        {
-            if(jokers[0].F == "J2")
-                swap(joker1,joker2);
-            cout << "Replace J1 with " << joker1 << " and J2 with " << joker2 << ".\n";
-        }
-        else if(j == 1)
-            cout << "Replace " << jokers[0].F << " with " << joker1 <<".\n";
-        else
-            cout << "There are no jokers.\n";
-        cout << "Put the first square to (" << ansy1 << ", " << ansx1 << ").\n";
-        cout << "Put the second square to (" << ansy2 << ", " << ansx2 << ").\n";
-    }
-    else
-        cout << "No solution.";
+    return squares;
 }
 int main()
 {
     cin >> n >> m;
     set<string>cards = pre(); // All Cards
+    vector<pair<string,pii>>jokers;
     for(int i = 1; i <= n; i++)
     {
         for(int j = 1; j <= m; j++)
@@ -147,19 +123,21 @@ int main()
                 jokers.pb({input[i][j],{i,j}});
         }
     }
-    squares = pre2();
+    map<pii,vector<pii>>squares = pre2();
     if(jokers.size() == 2)
     {
+        int y1,y2,x1,x2;
+        string j1 = "",j2 = "";
         bool done = false;
         for(auto c1:cards)
         {
-            joker1 = c1;
+            j1 = c1;
             input[jokers[0].S.F][jokers[0].S.S] = c1;
             for(auto c2:cards)
             {
                 if(c2 == c1)
                     continue;
-                joker2 = c2;
+                j2 = c2;
                 input[jokers[1].S.F][jokers[1].S.S] = c2;
                 for(auto i = squares.begin(); i != squares.end(); i++)
                 {
@@ -190,8 +168,8 @@ int main()
                         if(s1 && s2)
                         {
                             done = true;
-                            ansy1 = s1i, ansx1 = s1j;
-                            ansy2 = s2i, ansx2 = s2j;
+                            y1 = s1i, x1 = s1j;
+                            y2 = s2i, x2 = s2j;
                             break;
                         }
                     }
@@ -204,14 +182,27 @@ int main()
             if(done)
                 break;
         }
-        output(2,done);
+        if(done)
+        {
+            if(jokers[0].F == "J2")
+                swap(j1,j2);
+
+            cout << "Solution exists.\n";
+            cout << "Replace J1 with " << j1 << " and J2 with " << j2 << ".\n";
+            cout << "Put the first square to (" << y1 << ", " << x1 << ").\n";
+            cout << "Put the second square to (" << y2 << ", " << x2 << ").\n";
+        }
+        else
+            cout << "No solution.";
     }
     else if(jokers.size() == 1)
     {
+        int y1,y2,x1,x2;
+        string j1 = "";
         bool done = false;
         for(auto c1:cards)
         {
-            joker1 = c1;
+            j1 = c1;
             input[jokers[0].S.F][jokers[0].S.S] = c1;
             for(auto i = squares.begin(); i != squares.end(); i++)
             {
@@ -243,8 +234,8 @@ int main()
                     if(s1 && s2)
                     {
                         done = true;
-                        ansy1 = s1i, ansx1 = s1j;
-                        ansy2 = s2i, ansx2 = s2j;
+                        y1 = s1i, x1 = s1j;
+                        y2 = s2i, x2 = s2j;
                         break;
                     }
 
@@ -255,16 +246,28 @@ int main()
             if(done)
                 break;
         }
-        output(1,done);
+        if(done)
+        {
+            cout << "Solution exists.\n";
+            cout << "Replace " << jokers[0].F << " with " << j1 <<".\n";
+            cout << "Put the first square to (" << y1 << ", " << x1 << ").\n";
+            cout << "Put the second square to (" << y2 << ", " << x2 << ").\n";
+        }
+        else
+            cout << "No solution.";
     }
     else
     {
         if(squares.size())
         {
-            ansy1 = squares.begin()->F.F, ansx1 = squares.begin()->F.S;
-            ansy2 = squares.begin()->S[0].F, ansx2 = squares.begin()->S[0].S;
+            int y1 = squares.begin()->F.F, x1 = squares.begin()->F.S;
+            int y2 = squares.begin()->S[0].F, x2 = squares.begin()->S[0].S;
+            cout << "Solution exists.\n";
+            cout << "There are no jokers.\n";
+            cout << "Put the first square to (" << y1 << ", " << x1 << ").\n";
+            cout << "Put the second square to (" << y2 << ", " << x2 << ").\n";
         }
-        output(0,squares.size());
+        else
+            cout << "No solution.";
     }
-
 }
