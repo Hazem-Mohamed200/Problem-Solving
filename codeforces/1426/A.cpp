c9c91828f5f6bbc48624b0580550406881046766
Hazem Mohamed <hazemshata200@gmail.com>
2021-04-28 04:13:00 +0200

    Add solution for problem `A - Floor Number` Link: https://codeforces.com/contest/1426/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1426/submission/114409081

---
+++ codeforces/1426/A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define el '\n'
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pld pair<ld,ld>
+#define pff pair<float,float>
+#define F first
+#define S second
+#define vec(int) vector<int>
+#define pb push_back
+#define pf push_front
+#define mms(v,i) memset(v, i, sizeof v)
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+int dx[]= {0, 1, 0, -1};
+int dy[]= {1, 0, -1, 0};
+const ll N = 20; const ll mod = 1000000007;
+using namespace std;
+void file()
+{
+    freopen("input.txt","r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+}
+void zuka()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+int main()
+{
+    zuka();
+    int t; cin >> t;
+    while(t--)
+    {
+        ll n,x; cin >> n >> x;
+        if(n <= 2)
+            cout << 1 << el;
+        else
+        {
+            cout << ((n-3)/x)+2 << el;
+        }
+    }
+}
