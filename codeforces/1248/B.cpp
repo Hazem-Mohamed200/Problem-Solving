038afd2abb435a93ceb84aad79e7e3f34f94e556
Hazem Mohamed <hazemshata200@gmail.com>
2021-05-26 23:53:00 +0200

    Add solution for problem `B - Grow The Tree` Link: https://codeforces.com/contest/1248/problem/B Tags: greedy, math, sortings, *900 Ref: https://codeforces.com/contest/1248/submission/117446429

---
+++ codeforces/1248/B.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define el '\n'
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pld pair<ld,ld>
+#define pff pair<float,float>
+#define F first
+#define S second
+#define vec(int) vector<int>
+#define pb push_back
+#define pf push_front
+#define mms(v,i) memset(v, i, sizeof v)
+#define all(a) a.begin(), a.end()
+#define allr(a) a.rbegin(), a.rend()
+#define file freopen("input.txt","r" , stdin),freopen("output.txt" , "w" , stdout);
+#define Zuka  ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+int dx[] = {1, 0, 0, -1};
+int dy[] = {0, 1, -1, 0};
+int Dx[] = {-1, -1, -1,  0, 0,  1, 1,  1};
+int Dy[] = {-1,  0,  1, -1, 1, -1, 0,  1};
+
+const ll N = 2e5+5; const ll mod = 1e9+7;
+
+int main()
+{
+    Zuka
+    int n; cin >> n;
+    deque<ll>q;
+    vector<ll>v;
+    for(int i = 0; i < n; i++)
+    {
+        ll in; cin >> in;
+        v.push_back(in);
+    }
+    sort(allr(v));
+    for(int i = 0; i < n; i++)
+        q.push_back(v[i]);
+
+    ll x = 0, y = 0;
+    while(q.size())
+    {
+        x += q.front();
+        q.pop_front();
+        if(q.size())
+        {
+            y += q.back();
+            q.pop_back();
+        }
+    }
+    cout << x*x + y*y;
+
+}
