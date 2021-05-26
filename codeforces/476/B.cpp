2d6bd809273a90e8f8929c578ccee18eb1fab85e
Hazem Mohamed <hazemshata200@gmail.com>
2021-05-26 02:40:00 +0200

    Add solution for problem `B - Dreamoon and WiFi` Link: https://codeforces.com/contest/476/problem/B Tags: bitmasks, brute force, combinatorics, dp, math, probabilities, *1300 Ref: https://codeforces.com/contest/476/submission/117362257

--- codeforces/476/B.cpp
+++ codeforces/476/B.cpp
@@ -11,53 +11,58 @@ using namespace std;
 const ll INF = LONG_LONG_MAX;
 const ll N = 15; const ll mod = 1e9+7;
 
+int tot;
+string s1, s2;
+int dp[15][30];
+int target = 0;
+int pos = 0;
+int cnt = 0;
+ll fact(int n)
+{
+    if(n <= 1)
+        return 1;
+    else
+        return n * fact(n-1);
+}
+void solve()
+{
+
+    for(int i = 1; i <= 10; i++)
+    {
+        for(int p = 0; p <= i; p++)
+        {
+            int m = i-p;
+            dp[i][10 + (p-m)] = (fact(p+m)/(fact(p) * fact(m)));
+        }
+    }
+
+}
 int main()
 {
     Zuka
-    string s1, s2;
     cin >> s1 >> s2;
-    int target = 0;
-    int x = 0;
-    int pos = 0;
-
     for(int i = 0; i < s1.length(); i++)
     {
         if(s1[i] == '+')
             target++;
         else
             target--;
-        if(s2[i] == '?')
-            x++;
-        else if(s2[i] == '+')
+
+
+        if(s2[i] == '+')
             pos++;
-        else
+        else if(s2[i] == '-')
             pos--;
+        else
+            cnt++;
     }
-    int dp[30][30];
-    memset(dp, 0, sizeof dp);
-    // -4 -2 0 2 4
-    dp[0][s1.length()] = 1;
-    for(int i = 0; i < s1.length(); i++)
-    {
-        for(int j = 0; j <= 2*s1.length(); j++)
-        {
-            if(dp[i][j]) // ya3ny 0
-            {
-                if(s2[i] == '+')
-                {
-                    dp[i+1][j+1] += dp[i][j];
-                }
-                else if(s2[i] == '-')
-                {
-                    dp[i+1][j-1] += dp[i][j];
-                }
-                else
-                {
-                    dp[i+1][j+1] += dp[i][j];
-                    dp[i+1][j-1] += dp[i][j];
-                }
-            }
-        }
-    }
-    cout << fixed << setprecision(12) << (float)dp[s1.length()][s1.length() + target]/(float)(1 << x);
+    tot = 1 << cnt;
+    cout << fixed << setprecision(12);
+    solve();
+    if(cnt)
+        cout << (float)dp[cnt][10 + (target-pos)]/(float)tot;
+    else if(cnt == 0 && target == pos)
+        cout << (float)1;
+    else 
+        cout << (float )0;
 }
