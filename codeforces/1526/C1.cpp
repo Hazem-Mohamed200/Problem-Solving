6b7dec0ca1eebde26ad176ca64585776d59a422c
Hazem Mohamed <hazemshata200@gmail.com>
2021-05-28 19:29:00 +0200

    Add solution for problem `C1 - Potions (Easy Version)` Link: https://codeforces.com/contest/1526/problem/C1 Tags: brute force, data structures, dp, greedy, *1500 Ref: https://codeforces.com/contest/1526/submission/117688555

--- codeforces/1526/C1.cpp
+++ codeforces/1526/C1.cpp
@@ -6,54 +6,53 @@ using namespace std;
 #define pb push_back
 #define ll long long
 #define Zuka  ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-#define file freopen("input.txt", "r", stdin),freopen("output.txt", "wt", stdout);
+#define file freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
 
 const ll INF = LONG_LONG_MAX;
 const ll N = 2000+5; const ll mod = 1e9+7;
 
-int n;
-ll arr[N];
-ll dp[N][N];
-void solve()
+
+void testCase()
 {
-    memset(dp, -1, sizeof dp);
-    dp[0][0] = 0;
-    for(int i = 0; i <= n; i++)
+
+    int n; cin >> n;
+    priority_queue<ll>negs;
+    vector<ll>v;
+    ll hp = 0, ans = 0;
+    for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j <= i; j++)
+        ll in; cin >> in;
+        v.pb(in);
+    }
+    for(int i = 0; i < n; i++)
+    {
+        if(v[i] >= 0)
+            hp += v[i], ans++;
+        else
         {
-            if(!i || !j)
+            if(hp + v[i] >= 0)
             {
-                dp[i][j] = 0;
-                continue;
+                hp += v[i], ans++;
+                negs.push(-v[i]);
             }
             else
             {
-                if(dp[i-1][j-1] + arr[i] >= 0 && dp[i-1][j-1] >= 0)
-                    dp[i][j] = max(dp[i-1][j], dp[i-1][j-1] + arr[i]);
-                else
-                    dp[i][j] = dp[i-1][j];
+                if(negs.size() && -negs.top() < v[i])
+                {
+                    hp += negs.top();
+                    ans--;
+                    negs.pop();
+                    i--;
+                }
             }
         }
     }
-    int ans = 0;
-    for(int i = 0; i <= n; i++)
-        if(dp[n][i] != -1)
-            ans = i;
     cout << ans;
-}
-void testCase()
-{
-    cin >> n;
-    for(int i = 1; i <= n; i++)
-        cin >> arr[i];
-    solve();
 }
 int main()
 {
     Zuka
-    int t = 1;
-    //cin >> t;
+  int t = 1;
     while(t--)
-       testCase();
+        testCase();
 }
